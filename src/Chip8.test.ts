912ddd61958617410d2d97526a628d7961e35b2d
Igor Zoriy <me@igorzoriy.com>
2022-02-16 22:30:19 +0100

    Implement rom loading

--- src/Chip8.test.ts
+++ src/Chip8.test.ts
@@ -1,8 +1,26 @@
 import { Chip8 } from "./Chip8";
 
-test("chip8 is initialized after creating", () => {
-  const chip = new Chip8();
-  expect(chip.opcode).toBe(0);
-  expect(chip.I).toBe(0);
-  expect(chip.pc).toBe(0x200);
+const generateRom = (...bytes: number[]) => Uint8Array.of(...bytes).buffer;
+
+describe("Chip8", () => {
+  test("chip is initialized after creating", () => {
+    const chip = new Chip8();
+
+    expect(chip.memory.byteLength).toBe(4096);
+    expect(chip.opcode).toBe(0);
+    expect(chip.I).toBe(0);
+    expect(chip.pc).toBe(0x200);
+    expect(chip.v.byteLength).toBe(8);
+  });
+
+  test("a rom is being loaded to chip memory", () => {
+    const chip = new Chip8();
+    chip.loadRom(generateRom(0x00, 0xe0, 0xa2, 0x2a, 0x60, 0x0c, 0x61, 0x08));
+    const view = new DataView(chip.memory);
+    expect(view.getUint16(0x200)).toBe(0x00e0);
+    expect(view.getUint16(0x202)).toBe(0xa22a);
+    expect(view.getUint16(0x204)).toBe(0x600c);
+    expect(view.getUint16(0x206)).toBe(0x6108);
+    expect(view.getUint16(0x208)).toBe(0x0000);
+  });
 });
--- src/Chip8.ts
+++ src/Chip8.ts
@@ -1,20 +1,26 @@
 export class Chip8 {
-  memory: ArrayBuffer;
-  v: ArrayBuffer;
+  memory: ArrayBuffer = new ArrayBuffer(0);
   opcode: number = 0;
   I: number = 0;
   pc: number = 0;
+  v: ArrayBuffer = new ArrayBuffer(0);
 
   constructor() {
     this.initialize();
-    this.memory = new ArrayBuffer(4096);
-    this.v = new ArrayBuffer(8);
   }
 
   initialize() {
+    this.memory = new ArrayBuffer(0x1000);
     this.opcode = 0;
     this.I = 0;
     this.pc = 0x200;
+    this.v = new ArrayBuffer(8);
+  }
+
+  loadRom(rom: ArrayBuffer) {
+    const memoryArray = new Uint8Array(this.memory);
+    const romArray = new Uint8Array(rom);
+    memoryArray.set(romArray, 0x200);
   }
 
   performCycle() {
