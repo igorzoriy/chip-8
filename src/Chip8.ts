5731f59c446dc4da1074e03031a74cf4a9b91b8c
Igor Zoriy <me@igorzoriy.com>
2022-05-14 22:26:59 +0200

    Extract nextInstruction and skipNextInstruction methods

--- src/Chip8.ts
+++ src/Chip8.ts
@@ -35,6 +35,14 @@ export class Chip8 {
     return { x, y, nn, nnn };
   }
 
+  nextInstruction() {
+    this.pc += 2;
+  }
+
+  skipNextInstruction() {
+    this.pc += 4;
+  }
+
   performCycle() {
     const view = new DataView(this.memory);
     const opcode = view.getUint16(this.pc);
@@ -65,78 +73,78 @@ export class Chip8 {
         break;
       case 0x3000: // 3XNN
         if (this.vRegisters[x] === nn) {
-          this.pc += 4;
+          this.skipNextInstruction();
         } else {
-          this.pc += 2;
+          this.nextInstruction();
         }
         break;
       case 0x4000: // 4XNN
         if (this.vRegisters[x] !== nn) {
-          this.pc += 4;
+          this.skipNextInstruction();
         } else {
-          this.pc += 2;
+          this.nextInstruction();
         }
         break;
       case 0x5000: // 5XY0
         if (this.vRegisters[x] === this.vRegisters[y]) {
-          this.pc += 4;
+          this.skipNextInstruction();
         } else {
-          this.pc += 2;
+          this.nextInstruction();
         }
         break;
       case 0x6000: // 6XNN
         this.vRegisters[x] = nn;
-        this.pc += 2;
+        this.nextInstruction();
         break;
       case 0x7000: // 7XNN
         this.vRegisters[x] += nn;
-        this.pc += 2;
+        this.nextInstruction();
         break;
       case 0x8000: // 8XYN
         switch (opcode & 0x000f) {
           case 0x0000: // 8XY0
             this.vRegisters[x] = this.vRegisters[y];
-            this.pc += 2;
+            this.nextInstruction();
             break;
           case 0x0001: // 8XY1
             this.vRegisters[x] |= this.vRegisters[y];
-            this.pc += 2;
+            this.nextInstruction();
             break;
           case 0x0002: // 8XY2
             this.vRegisters[x] &= this.vRegisters[y];
-            this.pc += 2;
+            this.nextInstruction();
             break;
           case 0x0003: // 8XY3
             this.vRegisters[x] ^= this.vRegisters[y];
-            this.pc += 2;
+            this.nextInstruction();
             break;
           case 0x0004: // 8XY4
             const sum = this.vRegisters[x] + this.vRegisters[y];
             this.vRegisters[x] = sum;
             this.vRegisters[0xf] = sum > 0xff ? 1 : 0;
-            this.pc += 2;
+            this.nextInstruction();
             break;
           case 0x0005: // 8XY5
             const diff = this.vRegisters[x] - this.vRegisters[y];
             this.vRegisters[x] = diff;
             this.vRegisters[0xf] = diff < 0 ? 0 : 1;
-            this.pc += 2;
+            this.nextInstruction();
             break;
           case 0x0006: // 8XY6
             this.vRegisters[0xf] = this.vRegisters[x] & 0x1;
             this.vRegisters[x] >>= 1;
-            this.pc += 2;
+            this.nextInstruction();
             break;
           case 0x0007: // 8XY7
             const diff2 = this.vRegisters[y] - this.vRegisters[x];
             this.vRegisters[x] = diff2;
             this.vRegisters[0xf] = diff2 < 0 ? 0 : 1;
-            this.pc += 2;
+            this.nextInstruction();
             break;
           case 0x000e: // 8XYE
             this.vRegisters[0xf] = this.vRegisters[x] >> 7;
             this.vRegisters[x] <<= 1;
-            this.pc += 2;
+            this.nextInstruction();
             break;
           default:
             throw new Error(`Unknown opcode: ${opcode.toString(16)}`);
@@ -144,14 +152,14 @@ export class Chip8 {
         break;
       case 0x9000: // 9XY0
         if (this.vRegisters[x] !== this.vRegisters[y]) {
-          this.pc += 4;
+          this.skipNextInstruction();
         } else {
-          this.pc += 2;
+          this.nextInstruction();
         }
         break;
       case 0xa000: // ANNN
         this.I = opcode & 0x0fff;
-        this.pc += 2;
+        this.nextInstruction();
         break;
       case 0xb000: // BNNN
         this.pc = nnn + this.vRegisters[0];
