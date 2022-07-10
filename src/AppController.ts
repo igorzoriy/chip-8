67741ed4fed941600a2219213c89630d50b18673
Igor Zoriy <me@igorzoriy.com>
2022-07-10 21:24:21 +0200

    - Implement HEX keyboard (#7)
    - Implement EX9E, EXA1 opcodes

---
+++ src/AppController.ts
@@ -0,0 +1,71 @@
+import { ReactiveController, ReactiveControllerHost } from "lit";
+import { Chip8 } from "./Chip8";
+import { keymap } from "./keymap";
+
+export class AppController implements ReactiveController {
+  host: ReactiveControllerHost;
+  chip: Chip8;
+  private timer?: ReturnType<typeof setInterval>;
+  private keyboard: Uint8Array;
+
+  constructor(host: ReactiveControllerHost) {
+    (this.host = host).addController(this);
+    this.chip = new Chip8();
+    this.keyboard = new Uint8Array(16);
+  }
+
+  loadRom(buffer: ArrayBuffer) {
+    this.chip.loadRom(buffer);
+  }
+
+  run() {
+    let counter = 0;
+    this.timer = setInterval(() => {
+      counter++;
+
+      if (counter % 5 === 0) {
+        this.chip.tick();
+        counter = 0;
+      }
+      this.chip.performCycle();
+      this.host.requestUpdate();
+    }, 3);
+  }
+
+  getChipData() {
+    return this.chip.getData();
+  }
+
+  private handleKeyDown = ({ code }: KeyboardEvent) => {
+    const key = this.mapCodeToKeyboard(code);
+    if (key === -1) {
+      return;
+    }
+    this.keyboard[key] = 1;
+    this.chip.updateKeyboard(this.keyboard);
+  };
+
+  private handleKeyUp = ({ code }: KeyboardEvent) => {
+    const key = this.mapCodeToKeyboard(code);
+    if (key === -1) {
+      return;
+    }
+    this.keyboard[key] = 0;
+    this.chip.updateKeyboard(this.keyboard);
+  };
+
+  private mapCodeToKeyboard(code: string) {
+    return keymap.indexOf(code);
+  }
+
+  hostConnected() {
+    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener("keyup", this.handleKeyUp);
+  }
+
+  hostDisconnected() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+}
